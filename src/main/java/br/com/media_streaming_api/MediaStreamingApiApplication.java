90ced2c8b3866a4c4cb84ef91ec4f51a13c74a89
Felipe Estrela <felipeestrelatv@gmail.com>
2025-04-04 17:42:21 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/br/com/media_streaming_api/MediaStreamingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.media_streaming_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaStreamingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaStreamingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/media_streaming_api/MediaStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.media_streaming_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaStreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
